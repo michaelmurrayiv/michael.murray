2f6456f3212274dc3d3ad404675aa6f36e4e35d1
Michael Murray <michael.murray.iv@gmail.com>
2024-12-17 17:30:47 -0800

    added education section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -126,6 +126,21 @@ export default function AboutMe() {
           </div>
         </li>
       </ul>
+      <br></br>
+      <h2 className="text-2xl font-semibold mb-2">Education</h2>
+      <ul>
+        <li>
+          <h3 className="font-semibold">
+            Bachelor of Science in Computer Science
+          </h3>
+          <span className="text-sm text-gray-500">Sep 2020 – Mar 2025</span>
+          <p>Cal Poly, San Luis Obispo</p>
+          <p className="text-sm text-gray-600">Minor in Philosophy</p>
+          <p className="text-sm text-gray-600">
+            President's List (2020 – 2024)
+          </p>
+        </li>
+      </ul>
 
       <br></br>
 
