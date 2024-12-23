43c8a48d66a51d46e36618281a5b7e99e128baf3
Michael Murray <michael.murray.iv@gmail.com>
2024-12-23 11:58:40 -0800

    embedded chatbot demo on chatbot project page

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -1,10 +1,12 @@
 import TerminalChatBot from "@/components/TerminalChatBot";
+import React from "react";
+import YouTubeEmbed from "@/components/YouTubeEmbed";
 
 export default function ChatbotPage() {
   return (
     <div>
       <h1>Chatbot Page</h1>
-      <TerminalChatBot />
+      <YouTubeEmbed videoId="2Kctle8eEVY" />
     </div>
   );
 }
---
+++ src/components/YouTubeEmbed.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface YouTubeEmbedProps {
+  videoId: string;
+}
+
+const YouTubeEmbed: React.FC<YouTubeEmbedProps> = ({ videoId }) => {
+  return (
+    <div style={{ textAlign: "center", margin: "20px 0" }}>
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default YouTubeEmbed;
