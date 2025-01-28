b9088dcbf13a26fafa4fa352a1322a10b6b71df5
Michael Murray <michael.murray.iv@gmail.com>
2025-01-28 01:10:43 -0800

    updated website for compatibility with mobile devices

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,14 +3,14 @@ import TypewriterEffect from "@/components/TypewriterEffect";
 
 const Profile = () => {
   return (
-    <div className="flex">
-      <div className="w-1/2">
+    <div className="flex flex-col md:flex-row">
+      <div className="w-full md:w-1/2">
         <br />
         <TypewriterEffect text="Hi, I'm Michael" speed={100} />
         <p className="text-lg mt-2">I am a software engineer from Seattle.</p>
         <p className="text-lg mt-2">
           I&apos;m passionate about all things computer science, and am
-          particularyly interested in software engineering, deep learning, and
+          particularly interested in software engineering, deep learning, and
           cybersecurity. I enjoy working on projects where I can learn new
           technologies and solve interesting problems. In my free time, I love
           to play basketball, mountain bike, and read.
@@ -26,11 +26,11 @@ const Profile = () => {
           or via email at michael.murray.iv@gmail.com.
         </p>
       </div>
-      <div className="flex items-center justify-center w-1/2 mt-4">
+      <div className="flex items-center justify-center w-full md:w-1/2 mt-4 md:mt-0">
         <img
           src="/portrait.png"
           alt="Michael Murray"
-          className="h-60 w-60 rounded-full object-cover object-top"
+          className="h-60 w-60 rounded-full object-cover object-top hidden md:block"
         />
       </div>
     </div>
--- src/components/YouTubeEmbed.tsx
+++ src/components/YouTubeEmbed.tsx
@@ -6,10 +6,17 @@ interface YouTubeEmbedProps {
 
 const YouTubeEmbed: React.FC<YouTubeEmbedProps> = ({ videoId }) => {
   return (
-    <div style={{ textAlign: "center", margin: "20px 0" }}>
+    <div
+      className="relative"
+      style={{
+        paddingBottom: "56.25%",
+        height: 0,
+        overflow: "hidden",
+        maxWidth: "100%",
+      }}
+    >
       <iframe
-        width="560"
-        height="315"
+        className="absolute top-0 left-0 w-full h-full"
         src={`https://www.youtube.com/embed/${videoId}`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
